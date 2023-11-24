b772835b86c324d3648aec57896acc4d7d97dd1f
GregShriver <gshriver@udel.edu>
2023-11-24 11:42:55 -0500

    Edits to BirdNET.R, README.md, and Reality-Wren.rmd
    Added a Data folder

--- BirdNET.R
+++ BirdNET.R
@@ -1,12 +1,18 @@
-## Playing with BirdNET output
+## Playing with BirdNET output for the Carolina Wren Ecology Study
 
 ## load packages
 library(tidyverse)
 library(stringr)
 
+#########################################################################################################
+## Section one = get output files from BirdNET that were saved on the CANR Server (\\10.2.28.245\Shriver)
+##
+#########################################################################################################
+
+## This is not the right way to do this and should be changed to a relative path.
 setwd('Z:/RealityWren/BikePath/Site03')
 
-## create list of files from BirdNET output folder
+## create list of files from BirdNET output folder for the Site identified in the directory above
 list_of_files <- list.files(path = "./output",
                             recursive = TRUE,
                             pattern = "*.csv",
@@ -33,12 +39,12 @@ df$time <- as.numeric(df$time)
 ## add a count column (each row is one wren detection)
 df$count <- 1
 
-## save csv file so this can be the new start file
+## save csv file so this can be the new start file for visualization in Section 2 below
 write.csv(df, 'Z:/RealityWren/BikePath/results/Site_03_results.csv', row.names=FALSE)
 
 
-## add rows on days no wrens were detected
-x <- complete(df, day = min(day):max(day), fill = list(count = 0))
+## If necessary, add rows on days no wrens were detected 
+# x <- complete(df, day = min(day):max(day), fill = list(count = 0))
 
 
 ## summarize the number of wren detentions by day 
@@ -59,14 +65,7 @@ total_wren %>%
     )) +
   facet_grid(rows=vars(month))
 
-total_wren %>%
-  ggplot(aes(x = day, y = total_wren)) +
-  geom_bar(stat = "identity") +
-  theme_classic() +
-  labs(
-    x = "day",
-    y = "total wren detections",
-    title = paste(
-      "Reality Wren Phenology Mar-Oct Site 01"
-    )) +
-  facet_grid(cols=vars(month))
+#########################################################################################################
+## Section one = get output files from BirdNET that were saved on the CANR Server (\\10.2.28.245\Shriver)
+##
+#########################################################################################################
--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Repository for Carolina Wren ecology project
 
 
 Is R Studio connected to GitHub?
+
+I think it is...
+
