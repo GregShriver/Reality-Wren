939940b0158c89d61bfeb2e335b1388b92535c96
GregShriver <gshriver@udel.edu>
2023-12-04 17:25:13 -0500

    combinded BirdNET output from all sites and drafted a heat map

--- BirdNET.R
+++ BirdNET.R
@@ -6,7 +6,7 @@
 library(tidyverse)
 library(stringr)
 library(ggmap)
-
+library(lubridate)
 
 
 # Section I - get BirdNET output ----------------------------------------
@@ -14,7 +14,7 @@ library(ggmap)
 
 ## create list of files from BirdNET output folder for the Site you are processing.  You need to update the site number in 4 places; list_of_files, adding 'site' to the dataframe, write.csv, and ggplot code
 
-list_of_files <- list.files(path = "//10.2.28.245/Shriver/ARU_Data/RealityWren/BikePath/Site20/output",
+list_of_files <- list.files(path = "//10.2.28.245/Shriver/ARU_Data/RealityWren/BikePath/Site16/output",
                             recursive = TRUE,
                             pattern = "*.csv",
                             full.names = TRUE)
@@ -25,7 +25,7 @@ list_of_files <- list.files(path = "//10.2.28.245/Shriver/ARU_Data/RealityWren/B
 df <- readr::read_csv(list_of_files, id = "file_name")
 
 ## add side ID
-df$site <- 20
+df$site <- 16
 
 ## subtract year, month, day, and time from 'file_name'
 df$year <- substr(df$file_name, 67, 70)
@@ -43,7 +43,7 @@ df$time <- as.numeric(df$time)
 df$count <- 1
 
 ## save csv file so this can be the new start file for visualization in Section 2 below
-write.csv(df, 'Z:/RealityWren/BikePath/results/Site_20_results.csv', row.names=FALSE)
+write.csv(df, 'Z:/RealityWren/BikePath/results/Site_16_results.csv', row.names=FALSE)
 
 
 ## If necessary, add rows on days no wrens were detected 
@@ -64,7 +64,7 @@ total_wren %>%
     x = "day",
     y = "total wren detections",
     title = paste(
-      "Reality Wren Phenology Mar-Oct Site 20"
+      "Reality Wren Phenology Mar-Oct Site 16"
     )) +
   facet_grid(rows=vars(month))
 
@@ -76,16 +76,33 @@ total_wren %>%
 
 ## read BirdNET results files for each site into site specific data frame
 
-Site01 <- read.csv('../Reality-Wren/Data/Site_01_results.csv')
-Site02 <- read.csv('../Reality-Wren/Data/Site_02_results.csv')
-Site03 <- read.csv('../Reality-Wren/Data/Site_03_results.csv')
-Site04 <- read.csv('../Reality-Wren/Data/Site_04_results.csv')
-Site11 <- read.csv('../Reality-Wren/Data/Site_11_results.csv')
-Site12 <- read.csv('../Reality-Wren/Data/Site_12_results.csv')
-Site13 <- read.csv('../Reality-Wren/Data/Site_13_results.csv')
+Site01 <- read.csv('//10.2.28.245/Shriver/ARU_Data/RealityWren/BikePath/results/Site_01_results.csv')
+Site02 <- read.csv('//10.2.28.245/Shriver/ARU_Data/RealityWren/BikePath/results/Site_02_results.csv')
+Site03 <- read.csv('//10.2.28.245/Shriver/ARU_Data/RealityWren/BikePath/results/Site_03_results.csv')
+Site04 <- read.csv('//10.2.28.245/Shriver/ARU_Data/RealityWren/BikePath/results/Site_04_results.csv')
+Site05 <- read.csv('//10.2.28.245/Shriver/ARU_Data/RealityWren/BikePath/results/Site_05_results.csv')
+Site06 <- read.csv('//10.2.28.245/Shriver/ARU_Data/RealityWren/BikePath/results/Site_06_results.csv')
+Site07 <- read.csv('//10.2.28.245/Shriver/ARU_Data/RealityWren/BikePath/results/Site_07_results.csv')
+Site08 <- read.csv('//10.2.28.245/Shriver/ARU_Data/RealityWren/BikePath/results/Site_08_results.csv')
+Site09 <- read.csv('//10.2.28.245/Shriver/ARU_Data/RealityWren/BikePath/results/Site_09_results.csv')
+Site10 <- read.csv('//10.2.28.245/Shriver/ARU_Data/RealityWren/BikePath/results/Site_10_results.csv')
+Site11 <- read.csv('//10.2.28.245/Shriver/ARU_Data/RealityWren/BikePath/results/Site_11_results.csv')
+Site12 <- read.csv('//10.2.28.245/Shriver/ARU_Data/RealityWren/BikePath/results/Site_12_results.csv')
+Site13 <- read.csv('//10.2.28.245/Shriver/ARU_Data/RealityWren/BikePath/results/Site_13_results.csv')
+Site14 <- read.csv('//10.2.28.245/Shriver/ARU_Data/RealityWren/BikePath/results/Site_14_results.csv')
+Site15 <- read.csv('//10.2.28.245/Shriver/ARU_Data/RealityWren/BikePath/results/Site_15_results.csv')
+Site16 <- read.csv('//10.2.28.245/Shriver/ARU_Data/RealityWren/BikePath/results/Site_16_results.csv')
+Site17 <- read.csv('//10.2.28.245/Shriver/ARU_Data/RealityWren/BikePath/results/Site_17_results.csv')
+Site18 <- read.csv('//10.2.28.245/Shriver/ARU_Data/RealityWren/BikePath/results/Site_18_results.csv')
+Site19 <- read.csv('//10.2.28.245/Shriver/ARU_Data/RealityWren/BikePath/results/Site_19_results.csv')
+Site20 <- read.csv('//10.2.28.245/Shriver/ARU_Data/RealityWren/BikePath/results/Site_20_results.csv')
+
+
+
 
 ## rbind site specific data frames into one data frame (song.dat)
-song.dat <- rbind(Site01, Site02, Site03, Site11, Site12)
+
+song.dat <- rbind(Site01, Site02, Site03, Site04, Site05, Site06, Site07, Site08, Site09, Site10, Site11, Site12, Site13, Site14, Site15, Site16, Site17, Site18, Site19, Site20)
 
 ## save combined file that includes all wren detections for all sites and months
 
@@ -98,6 +115,28 @@ write.csv(song.dat, '../Reality-Wren/Output/wren_detections.csv', row.names=FALS
 song.dat <- song.dat %>%
   mutate(time_code = case_when(time < 1200 ~ 'am',
                                time > 1200 ~ 'pm'))
+
+
+## Dealing with creating an actual date field from day, month, year
+song.dat$day <- as.integer(song.dat$day)
+
+song.dat$day <- sprintf("%02d", song.dat$day)
+
+song.dat$month <- as.integer(song.dat$month)
+
+song.dat$month <- sprintf("%02d", song.dat$month)
+
+song.dat$date <- paste0(song.dat$year, song.dat$month, song.dat$day)
+
+song.dat$date <- as.numeric(song.dat$date)
+
+song.dat$date <- ymd(song.dat$date)
+
+
+## Create a year day from the newly created date field
+
+song.dat$yday <- yday(song.dat$date)
+
 
 
 ## Create df grouped by time_code (am & pm)
@@ -126,7 +165,7 @@ my_plot_hours <- function(sites){
       y = "total wren detections",
       title = paste("Reality Wren Phenology Mar-Oct Site", sites)) +
     facet_grid(rows=vars(month))
-  ggsave(filename=paste0(sites,".png"))
+  ggsave(path='../Reality-Wren/Output/', filename=paste0(sites,".png"))
 }
 
 
@@ -136,52 +175,110 @@ lapply(sites, my_plot_hours)
 
 
 
-## older graphing code-----------------
-## visualize with ggplot
 
-## try to iterate through with a for loop
-for (i in levels(total_wren$site)) {
-  p <- total_wren %>%
-    filter(site == i) %>%
-    ggplot(aes(x = day, y = total_wren)) +
-    geom_bar(stat = "identity") +
+
+
+# Heatmap  ----------------------------------------------------------------
+
+## Create df grouped by year day
+
+total_wren_yday <- song.dat %>% 
+  group_by(site, yday) %>%
+  summarize(total_wren = sum(count))
+
+
+total_wren_day$site <- as.factor(total_wren_yday$site)
+
+site <- total_wren_yday$site
+
+## create the heat map
+
+ggplot(total_wren_yday, aes(x = yday, y = site, fill = total_wren)) +
+  geom_tile() +
     theme_classic() +
-    labs(
-      x = "day",
-      y = "total wren detections",
-      title = paste("Reality Wren Phenology Mar-Oct Site", i)) +
-    facet_grid(rows=vars(month))
-    print(p)
-    ggsave(p, filename = paste0("site", i, ".png"))
-    
-}
+   scale_fill_viridis() + 
+   scale_y_continuous(breaks = site)
 
 
-## now try a Functional Approach which seems better!
 
-## create the plotting function that lapply will iterate through 
-
-my_plot <- function(sites){
-  total_wren %>%
-    filter(site == sites) %>%
-    ggplot(aes(x = day, y = total_wren)) +
-    geom_bar(stat = "identity") +
-    theme_classic() +
-    labs(
-      x = "day",
-      y = "total wren detections",
-      title = paste("Reality Wren Phenology Mar-Oct Site", sites)) +
-    facet_grid(rows=vars(month))
-  # ggsave(p, filename = paste0("site", i, ".png"))
-}
-
-## create the vector of sites 
-sites <- levels(total_wren$site)
-
-## run the vector of sites through the function using lapply
-lapply(sites, my_plot)
+## Example code to break heat map figs by month??
 
 
+
+library(ggplot2)
+library(dplyr) # easier data wrangling 
+library(viridis) # colour blind friendly palette, works in B&W also
+library(Interpol.T) #  will generate a large dataset on initial load
+library(lubridate) # for easy date manipulation
+library(ggExtra) # because remembering ggplot theme options is beyond me
+library(tidyr) 
+
+
+data <- data(Trentino_hourly_T,package = "Interpol.T")
+
+names(h_d_t)[1:5]<- c("stationid","date","hour","temp","flag")
+df <- tbl_df(h_d_t) %>%
+  filter(stationid =="T0001")
+
+df <- df %>% mutate(year = year(date),
+                    month = month(date, label=TRUE),
+                    day = day(date))
+
+df$date<-ymd(df$date) # not necessary for plot but 
+#useful if you want to do further work with the data
+
+#cleanup
+rm(list=c("h_d_t","mo_bias","Tn","Tx",
+          "Th_int_list","calibration_l",
+          "calibration_shape","Tm_list"))
+
+
+#create plotting df
+df <-df %>% select(stationid,day,hour,month,year,temp)%>%
+  fill(temp) #optional - see note below
+
+# Re: use of fill
+# This code is for demonstrating a visualisation technique
+# There are 5 missing hourly values in the dataframe.
+
+# see the original plot here (from my ggplot demo earlier this year) to see the white spaces where the missing values occcur:
+# https://github.com/johnmackintosh/ggplotdemo/blob/master/temp8.png 
+
+# I used 'fill' from  tidyr to take the prior value for each missing value and replace the NA
+# This is a quick fix for the blog post only - _do not_ do this with your real world data
+
+# Should really use either use replace_NA or complete(with fill)in tidyr 
+# OR 
+# Look into more specialist way of replacing these missing values -e.g. imputation.
+
+
+
+statno <-unique(df$stationid)
+
+
+
+######## Plotting starts here#####################
+p <-ggplot(df,aes(day,hour,fill=temp))+
+  geom_tile(color= "white",size=0.1) + 
+  scale_fill_viridis(name="Hrly Temps C",option ="C")
+p <-p + facet_grid(year~month)
+p <-p + scale_y_continuous(trans = "reverse", breaks = unique(df$hour))
+p <-p + scale_x_continuous(breaks =c(1,10,20,31))
+p <-p + theme_minimal(base_size = 8)
+p <-p + labs(title= paste("Hourly Temps - Station",statno), x="Day", y="Hour Commencing")
+p <-p + theme(legend.position = "bottom")+
+  theme(plot.title=element_text(size = 14))+
+  theme(axis.text.y=element_text(size=6)) +
+  theme(strip.background = element_rect(colour="white"))+
+  theme(plot.title=element_text(hjust=0))+
+  theme(axis.ticks=element_blank())+
+  theme(axis.text=element_text(size=7))+
+  theme(legend.title=element_text(size=8))+
+  theme(legend.text=element_text(size=6))+
+  removeGrid()#ggExtra
+
+# you will want to expand your plot screen before this bit!
+p #awesomeness
 
 
 
